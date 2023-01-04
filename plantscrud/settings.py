17e5a06707befe581ca927fc5d6c2fae1894da56
FelipeGCx <ggfelipegc@gmail.com>
2023-01-03 20:23:43 -0500

    added postgreSQL database

--- plantscrud/settings.py
+++ plantscrud/settings.py
@@ -123,10 +123,21 @@ WSGI_APPLICATION = "plantscrud.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'imEd5mGRtnw59rL7itiw',
+        'HOST': 'containers-us-west-34.railway.app',
+        'PORT': '7326',
     }
 }
 
