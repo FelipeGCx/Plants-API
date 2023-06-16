d0d7f8b5153151cc719346640241c3ee5e38d68c
felipe gomez <ggfelipegc@gmail.com>
2023-06-16 18:05:02 -0500

    refactor remake filters

--- plants/views/crystalsStockUserView.py
+++ plants/views/crystalsStockUserView.py
@@ -6,76 +6,136 @@ from rest_framework.views import APIView
 
 import math
 
+
 class CrystalStockUserView(APIView):
+    class item:
+        pass
+
+    class Data:
+        results = []
     page_size = 30
     page = 1
-    
+
+    def get_crystal(self, obj):
+        crystals = Crystal.objects.get(id=int(obj.__dict__["id_crystal_id"]))
+        return crystals.__dict__
+
+    def add_crystal(self, obj):
+        item = self.item()
+        item.id = obj.__dict__["id"]
+        item.quantity = obj.__dict__["quantity"]
+        item.price = obj.__dict__["price"]
+        item.state = obj.__dict__["state"]
+        crystal = self.get_crystal(obj)
+        item.name = crystal.get("name")
+        description = crystal.get("description")
+        item.description = description
+        vibration = crystal.get("vibration")
+        item.vibration = vibration
+        benefits = crystal.get("benefits")
+        item.benefits = benefits
+        properties = crystal.get("properties")
+        item.properties = properties
+        zodiac = crystal.get("zodiac")
+        item.zodiac = zodiac
+        planets = crystal.get("planets")
+        item.planets = planets
+        elements = crystal.get("elements")
+        item.elements = elements
+        chakras = crystal.get("chakras")
+        item.chakras = chakras
+        image_crystal = crystal.get("image_crystal")
+        item.image_crystal = image_crystal
+        image_gemstone = crystal.get("image_gemstone")
+        item.image_gemstone = image_gemstone
+        created_at = crystal.get("created_at")
+        item.created_at = created_at
+        return item
+
     def get(self, request, user=None, format=None):
-        page = int(request.query_params.get('page', None)) if request.query_params.get('page', None) is not None else self.page
-        page_size = int(request.query_params.get('page_size', None)) if request.query_params.get('page_size', None) is not None else self.page_size
-            
+        page = int(request.query_params.get('page', None)) if request.query_params.get(
+            'page', None) is not None else self.page
+        page_size = int(request.query_params.get('page_size', None)) if request.query_params.get(
+            'page_size', None) is not None else self.page_size
+
         crystalStock = CrystalStock.objects.all()
-        
+        crystalisStock = self.Data()
+        crystalisStock.results = [
+            self.add_crystal(obj) for obj in crystalStock]
+        crystalisStock = crystalisStock.results
+
         name = self.request.query_params.get('name', None)
         if name is not None:
-            crystalStock = crystalStock.filter(name__contains = name) 
-            
+            crystalStock = list(
+                filter(lambda x: name.lower() in x.name.lower(), crystalisStock))
+
         vibration = self.request.query_params.get('vibration', None)
         if vibration is not None:
-            crystalStock = crystalStock.filter(vibration = vibration) 
-            
+            crystalisStock = list(filter(lambda x: int(vibration) in x.vibration, crystalisStock)
+                                  )
+
         zodiac = self.request.query_params.get('zodiac', None)
         if zodiac is not None:
             zodiac = zodiac.split(",")
             for z in zodiac:
-                crystalStock =  crystalStock.filter(zodiac__contains=z)
-        
+                crystalisStock = list(
+                    filter(lambda x: z.lower() in x.zodiac.lower(), crystalisStock))
+
         planets = self.request.query_params.get('planets', None)
         if planets is not None:
             planets = planets.split(",")
             for p in planets:
-                crystalStock =  crystalStock.filter(planets__contains=p)
-                
+                crystalisStock = list(
+                    filter(lambda x: p.lower()
+                           in x.planets.lower(), crystalisStock)
+                )
+
         chakras = self.request.query_params.get('chakras', None)
         if chakras is not None:
             chakras = chakras.split(",")
             for c in chakras:
-                crystalStock =  crystalStock.filter(chakras__contains=c)
-                
+                crystalisStock = list(
+                    filter(lambda x: c.lower()
+                           in x.chakras.lower(), crystalisStock)
+                )
+
         elements = self.request.query_params.get('elements', None)
         if elements is not None:
             elements = elements.split(",")
             for e in elements:
-                crystalStock =  crystalStock.filter(elements__contains=e)
-                
+                crystalisStock = list(
+                    filter(lambda x: e.lower()
+                           in x.elements.lower(), crystalisStock)
+                )
+
         data = []
-        for item in crystalStock:
-            crystal = Crystal.objects.get(id=item.id)
-            fav = CrystalFavorite.objects.filter(id_user= user,id_crystal = item.id, )
+        for item in crystalisStock:
+            fav = CrystalFavorite.objects.filter(
+                id_user=user, id_crystal=item.id, )
             val = {
-                "id":item.id,
-                "name": crystal.name,
-                "description": crystal.description,
-                "vibration": crystal.vibration,
-                "benefits": [b.removeprefix(" ") for b in crystal.benefits.split(",")],
-                "properties": [p.removeprefix(" ") for p in crystal.properties.split(",")],
-                "zodiac": [z.removeprefix(" ") for z in crystal.zodiac.split(",")],
-                "planets": [p.removeprefix(" ") for p in crystal.planets.split(",")],
-                "elements": [e.removeprefix(" ") for e in crystal.elements.split(",")],
-                "chakras": [c.removeprefix(" ") for c in crystal.chakras.split(",")],
-                "imageCrystal": crystal.image_crystal,
-                "imageGemstone": crystal.image_gemstone,
+                "id": item.id,
+                "name": item.name,
+                "description": item.description,
+                "vibration": item.vibration,
+                "benefits": [b.removeprefix(" ") for b in item.benefits.split(",")],
+                "properties": [p.removeprefix(" ") for p in item.properties.split(",")],
+                "zodiac": [z.removeprefix(" ") for z in item.zodiac.split(",")],
+                "planets": [p.removeprefix(" ") for p in item.planets.split(",")],
+                "elements": [e.removeprefix(" ") for e in item.elements.split(",")],
+                "chakras": [c.removeprefix(" ") for c in item.chakras.split(",")],
+                "imageCrystal": item.image_crystal,
+                "imageGemstone": item.image_gemstone,
                 "quantity": item.quantity,
                 "price": item.price,
                 "state": item.state,
-                "favorite": True if fav else False  
+                "favorite": True if fav else False
             }
             data.append(val)
         total_items = len(data)
         total_pages = math.ceil(total_items/page_size)
         l_item = page*page_size
         f_item = l_item - page_size
-        
+
         paginated_data = data[f_item:l_item]
         final_data = {
             "totalItems": total_items,
@@ -84,4 +144,3 @@ class CrystalStockUserView(APIView):
             "results": paginated_data
         }
         return Response(final_data, status=200)
-
--- plants/views/plantsStockUserView.py
+++ plants/views/plantsStockUserView.py
@@ -5,20 +5,22 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 import math
 
+
 class PlantsStockUserView(APIView):
     class item:
-        pass   
+        pass
+
     class Data:
-        results=[]
+        results = []
     page_size = 30
     page = 1
-    
+
     def get_plant(self, obj):
-        # id_plant = obj.id_plant.replace("Plant object (","").replace(")","") 
+        # id_plant = obj.id_plant.replace("Plant object (","").replace(")","")
         plants = Plant.objects.get(id=int(obj.__dict__["id_plant_id"]))
         return plants.__dict__
-    
-    def add_plant(self,obj):
+
+    def add_plant(self, obj):
         item = self.item()
         item.id = obj.__dict__["id"]
         item.quantity = obj.__dict__["quantity"]
@@ -26,7 +28,7 @@ class PlantsStockUserView(APIView):
         item.price = obj.__dict__["price"]
         item.state = obj.__dict__["state"]
         plant = self.get_plant(obj)
-        item.name =  plant.get("name")
+        item.name = plant.get("name")
         other_names = plant.get("other_names")
         item.other_names = other_names
         description = plant.get("description")
@@ -47,78 +49,88 @@ class PlantsStockUserView(APIView):
         item.flowering = flowering
         size = plant.get("size")
         item.size = size
-        image_front =  plant.get("image_front")
-        item.image_front =  image_front
-        render =  plant.get("render")
-        item.render =  render
-        inside =  plant.get("inside")
-        item.inside =  inside
-        created_at =  plant.get("created_at")
-        item.created_at =  created_at
+        image_front = plant.get("image_front")
+        item.image_front = image_front
+        render = plant.get("render")
+        item.render = render
+        inside = plant.get("inside")
+        item.inside = inside
+        created_at = plant.get("created_at")
+        item.created_at = created_at
         return item
-    
+
     def get(self, request, user=None, format=None):
-        page = int(request.query_params.get('page', None)) if request.query_params.get('page', None) is not None else self.page
-        page_size = int(request.query_params.get('page_size', None)) if request.query_params.get('page_size', None) is not None else self.page_size
-            
+        page = int(request.query_params.get('page', None)) if request.query_params.get(
+            'page', None) is not None else self.page
+        page_size = int(request.query_params.get('page_size', None)) if request.query_params.get(
+            'page_size', None) is not None else self.page_size
+
         plantStock = PlantStock.objects.all()
         plantisStock = self.Data()
         plantisStock.results = [self.add_plant(obj) for obj in plantStock]
         plantisStock = plantisStock.results
-        
+
         name = self.request.query_params.get('name', None)
         if name is not None:
-            # plantisStock = plantisStock.filter(name__contains = name) or plantisStock.filter(other_names__contains = name) 
-            plantisStock = list(filter(lambda x: name.lower() in x.name.lower() or name.lower() in x.other_names.lower(), plantisStock))
-            
+            # plantisStock = plantisStock.filter(name__contains = name) or plantisStock.filter(other_names__contains = name)
+            plantisStock = list(filter(lambda x: name.lower() in x.name.lower(
+            ) or name.lower() in x.other_names.lower(), plantisStock))
+
         species = self.request.query_params.get('species', None)
         if species is not None:
-            # plantisStock = plantisStock.filter(species = species) 
-            plantisStock = list(filter(lambda x: x.species.lower() == species.lower(), plantisStock))
-            
+            # plantisStock = plantisStock.filter(species = species)
+            plantisStock = list(
+                filter(lambda x: x.species.lower() == species.lower(), plantisStock))
+
         group = self.request.query_params.get('group', None)
         if group is not None:
-            # plantisStock = plantisStock.filter(group = group) 
-            plantisStock = list(filter(lambda x: x.group.lower() == group.lower(), plantisStock))
-        
+            # plantisStock = plantisStock.filter(group = group)
+            plantisStock = list(
+                filter(lambda x: x.group.lower() == group.lower(), plantisStock))
+
         irrigation = self.request.query_params.get('irrigation', None)
         if irrigation is not None:
             irrigation = irrigation.split(",")
             for i in irrigation:
                 # plantisStock =  plantisStock.filter(irrigation__contains=i)
-                plantisStock = list(filter(lambda x: i.lower() in x.irrigation.lower(), plantisStock))
-                                                    
+                plantisStock = list(
+                    filter(lambda x: i.lower() in x.irrigation.lower(), plantisStock))
+
         flowering = self.request.query_params.get('flowering', None)
         if flowering is not None:
             flowering = eval(flowering.capitalize())
             # plantisStock = plantisStock.filter(flowering = flowering)
-            plantisStock = list(filter(lambda x: x.flowering == flowering, plantisStock))
-            
+            plantisStock = list(
+                filter(lambda x: x.flowering == flowering, plantisStock))
+
         inside = self.request.query_params.get('inside', None)
         if inside is not None:
             inside = eval(inside.capitalize())
             # plantisStock = plantisStock.filter(inside = inside)
-            plantisStock = list(filter(lambda x: x.inside == inside, plantisStock))
-            
+            plantisStock = list(
+                filter(lambda x: x.inside == inside, plantisStock))
+
         price_first = self.request.query_params.get('priceFirst', None)
         if price_first is not None:
             price_second = self.request.query_params.get('priceSecond', None)
             if price_second is not None:
                 # plantisStock =  plantisStock.filter(price__gte=price_first,price__lte=price_second)
-                plantisStock = list(filter(lambda x: x.price >= int(price_first) and x.price <= int(price_second), plantisStock))
-            
+                plantisStock = list(filter(lambda x: x.price >= int(
+                    price_first) and x.price <= int(price_second), plantisStock))
+
         # order = self.request.query_params.get('order', None)
         # if order is not None:
         #     # plantisStock = plantisStock.order_by(order)
         #     # plantisStock = sorted(plantisStock, key=lambda x: x["price"], reverse=True if "-" in order else False)
         #     plantisStock = plantisStock.sort(key=lambda x: x.price, reverse=True if "-" in order else False)
-        
+
         data = []
         for item in plantisStock:
             if item.state:
-                fav = PlantFavorite.objects.filter(id_user= user,id_plant = item.id)
+                fav = PlantFavorite.objects.filter(
+                    id_user=user, id_plant=item.id)
                 val = {
-                    "id":item.id,
+                    "id": item.id,
                     "name": item.name,
                     "otherNames": [n.removeprefix(" ") for n in item.other_names.split(",")],
                     "description": item.description,
@@ -138,18 +150,19 @@ class PlantsStockUserView(APIView):
                     "discount": item.discount,
                     "createdAt": item.created_at,
                     "state": item.state,
-                    "favorite": True if fav else False  
+                    "favorite": True if fav else False
                 }
                 data.append(val)
-                
+
         order = self.request.query_params.get('order', None)
         if order is not None:
-            data = sorted(data, key=lambda x: x[str(order).replace("-","")], reverse=True if "-" in order else False)
+            data = sorted(data, key=lambda x: x[str(order).replace(
+                "-", "")], reverse=True if "-" in order else False)
         total_items = len(data)
         total_pages = math.ceil(total_items/page_size)
         l_item = page*page_size
         f_item = l_item - page_size
-        
+
         paginated_data = data[f_item:l_item]
         final_data = {
             "totalItems": total_items,
