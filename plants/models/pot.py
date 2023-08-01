f336ee543b83e1ad15a65435b6f134cbebafe2a4
felipe gomez <ggfelipegc@gmail.com>
2023-08-01 11:22:23 -0500

    feat: add size field

--- plants/models/pot.py
+++ plants/models/pot.py
@@ -5,5 +5,6 @@ class Pot(models.Model):
     name = models.CharField(max_length=210)
     price = models.BigIntegerField()
     image = models.TextField()
+    size = models.TextField(default="")
     render = models.TextField()
     quantity = models.IntegerField()
