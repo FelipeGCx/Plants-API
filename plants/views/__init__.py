4e94bc20bf9cf10de42f6d90cf81a6124cff3827
felipe gomez <ggfelipegc@gmail.com>
2023-08-21 16:23:42 -0500

    feat: add new views imports

--- plants/views/__init__.py
+++ plants/views/__init__.py
@@ -2,3 +2,6 @@ from .verifyTokenView import VerifyTokenView
 from .crystalsStockUserView import CrystalStockUserView
 from .plantsStockUserView import PlantsStockUserView
 from .speciesView import SpeciesView
+from .plantView import PlantView
+from .plantStockView import PlantStockView
+from .plantStockFavView import PlantStockFavView
