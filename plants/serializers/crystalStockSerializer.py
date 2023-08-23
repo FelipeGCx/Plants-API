1c13a560546107482594ba96bb8a5e45fce18c41
felipe gomez <ggfelipegc@gmail.com>
2023-08-23 15:49:39 -0500

    refactor: update serializer logic

--- plants/serializers/crystalStockSerializer.py
+++ plants/serializers/crystalStockSerializer.py
@@ -1,33 +1,16 @@
 from rest_framework import serializers
-from plants.models.crystal import Crystal
-from plants.models.crystalStock import CrystalStock
+from plants.models import Crystal
+from plants.models import CrystalStock
+from plants.serializers import CrystalSerializer
 
 class CrystalStockSerializer(serializers.ModelSerializer):
     class Meta:
-        model= CrystalStock
-        fields = ["id","id_crystal", "quantity", "price", "state"]
-        
-    class Meta:
-        model= Crystal
-        fields = ["id","name","description","vibration","benefits","properties","zodiac","planets","elements","chakras","image_crystal","image_gemstone"]
-        
-    def to_representation(self, obj):
-        crystal = Crystal.objects.get(id=obj.id)
-        crystalStock = CrystalStock.objects.get(id=obj.id)
-        return {
-            "id": crystal.id,
-            "name": crystal.name,
-            "description": crystal.description,
-            "vibration": crystal.vibration,
-            "benefits": [b.removeprefix(" ") for b in crystal.benefits.split(",")],
-            "properties": [p.removeprefix(" ") for p in crystal.properties.split(",")],
-            "zodiac": [z.removeprefix(" ") for z in crystal.zodiac.split(",")],
-            "planets": [p.removeprefix(" ") for p in crystal.planets.split(",")],
-            "elements": [e.removeprefix(" ") for e in crystal.elements.split(",")],
-            "chakras": [c.removeprefix(" ") for c in crystal.chakras.split(",")],
-            "imageCrystal": crystal.image_crystal,
-            "imageGemstone": crystal.image_gemstone,
-            "quantity": crystalStock.quantity,
-            "price": crystalStock.price,
-            "state": crystalStock.state
-        }
+        model = CrystalStock
+        fields = "__all__"
+
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        crystal_rep = CrystalSerializer(instance.id_crystal).data
+        rep.update(crystal_rep)
+        rep.pop('id_crystal', None)
+        return rep
