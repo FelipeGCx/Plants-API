b243930c7d6e83fa80b00c2421b9521d2e185b50
felipe gomez <ggfelipegc@gmail.com>
2023-08-28 11:19:35 -0500

    feat: add user plants stock favorite

--- plants/views/__init__.py
+++ plants/views/__init__.py
@@ -5,6 +5,7 @@ from .speciesView import SpeciesView
 from .plantView import PlantView
 from .plantStockView import PlantStockView
 from .plantStockFavView import PlantStockFavView
+from .plantStockFavoriteView import PlantStockFavoriteView
 from .crystalView import CrystalView
 from .crystalStockView import CrystalStockView
 from .crystalStockFavView import CrystalStockFavView
